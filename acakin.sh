e46dcf6d148ec29c2b95d014e921e97e50afb8ae
lanandra <luthfi.anandra@gmail.com>
2020-12-19 22:35:25 +0700

    initial commit

---
+++ acakin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+d1=$(( $RANDOM % 10 ))
+d2=$(( $RANDOM % 10 ))
+d3=$(( $RANDOM % 10 ))
+d4=$(( $RANDOM % 10 ))
+d5=$(( $RANDOM % 10 ))
+d6=$(( $RANDOM % 10 ))
+
+echo "$d1 $d2 $d3 $d4 $d5 $d6"
